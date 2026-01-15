c3af15fdc106d879d4fb62387b8c5fa5f7b37315
Gina Rahmawati <gina10222088@sttcipasung.ac.id>
2026-01-15 23:28:10 +0700

    parki.kt

---
+++ app/src/main/java/com/example/parkirkampus/parkir.kt
@@ -0,0 +1,9 @@
+package com.example.parkirkampus
+
+data class parkir(
+    val plat: String,
+    val jenis: String,
+    val jam_masuk: String,
+    val jam_keluar: String,
+    val total_bayar: Int
+)
