9bfaf00e8a287ca23214931765b3c78bbf85017e
Gina Rahmawati <gina10222088@sttcipasung.ac.id>
2026-01-18 11:39:06 +0700

    delete project

--- app/src/main/java/com/example/parkirkampus/parkir.kt
+++
@@ -1,9 +0,0 @@
-package com.example.parkirkampus
-
-data class Parkir(
-    val plat: String,
-    val jenis: String,
-    val jam_masuk: String,
-    val jam_keluar: String,
-    val total_bayar: Int
-)
